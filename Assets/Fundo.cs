152ff245a79ab11d70ba1c334cb8aa91bd91203e
Bernardo Alvim <alvim103@gmail.com>
2023-07-12 12:55:54 -0300

    adicionando o fundo animado

--- Assets/Fundo.cs
+++ Assets/Fundo.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Fundo : MonoBehaviour
 {
+    [SerializeField] private Texture2D[] textures;
+    private int currentTextureIndex = 0;
+    private SpriteRenderer spriteRenderer;
+    private float timer = 0f;
+    public float textureChangeInterval = 0.5f;
 
-    public Texture2D[] textures;
-
-    // Start is called before the first frame update
-    void Start() {
-        
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (textures.Length > 0)
+        {
+            spriteRenderer.sprite = Sprite.Create(textures[currentTextureIndex], new Rect(0, 0, textures[currentTextureIndex].width, textures[currentTextureIndex].height), Vector2.one * 0.5f);
+        }
     }
 
-    // Update is called once per frame
-    void Update() {
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= textureChangeInterval)
+        {
+            ChangeTexture();
+            timer = 0f;
+        }
+    }
+
+    private void ChangeTexture()
+    {
+        currentTextureIndex++;
+        if (currentTextureIndex >= textures.Length)
+        {
+            currentTextureIndex = 0;
+        }
+
+        spriteRenderer.sprite = Sprite.Create(textures[currentTextureIndex], new Rect(0, 0, textures[currentTextureIndex].width, textures[currentTextureIndex].height), Vector2.one * 0.5f);
     }
 }
